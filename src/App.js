20d989cc599756c0771a3546831dd2464e985f16
bsonatunde <onatunde.samuel@gmail.com>
2023-03-02 15:23:42 +0100

    Rogue Game

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactRogue from "./ReactRogue";
+
+function App() {
+  return (
+    <div className="App">
+      <ReactRogue/>
+     
+    </div>
+  );
+}
+
+export default App;
